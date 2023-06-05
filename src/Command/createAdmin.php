1d82401b4a9cf0ddbe221bdabce08fab13cc2534
Christine Chau <christinemiko@gmail.com>
2023-06-05 11:56:13 +0200

    add command create sportscaster

---
+++ src/Command/createAdmin.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'app:create-admin',
+    description: 'Add a short description for your command',
+)]
+class createAdmin extends Command
+{
+    private EntityManagerInterface $entityManager;
+    private UserPasswordHasherInterface $userPasswordHasher;
+
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->entityManager = $entityManager;
+        $this->userPasswordHasher = $userPasswordHasher;
+
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('email', InputArgument::REQUIRED, 'Email')
+            ->addArgument('password', InputArgument::REQUIRED, 'Password')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $email = $input->getArgument('email');
+        $password = $input->getArgument('password');
+
+        $user = new User();
+        $user->setLastName('admin');
+        $user->setFirstName('admin');
+        $user->setEmail($email);
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, $password));
+        $user->setRoles(["ROLE_ADMIN"]);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $io->success('User created.');
+
+        return Command::SUCCESS;
+    }
+}
