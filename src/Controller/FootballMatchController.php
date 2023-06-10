04437013be73b691174cf73a4152ef0367369e62
Christine Chau <christinemiko@gmail.com>
2023-06-10 21:40:58 +0200

    add OneMatchWatch with id match

--- src/Controller/FootballMatchController.php
+++ src/Controller/FootballMatchController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\FootballMatch;
+use App\Repository\FootballMatchRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 class FootballMatchController extends AbstractController
 {
@@ -14,5 +18,48 @@ class FootballMatchController extends AbstractController
         return $this->render('football_match/index.html.twig', [
             'controller_name' => 'FootballMatchController',
         ]);
+    }
+
+    #[Route('allmatcheswatch', name:'visualiserlesmatchs')]
+    public function Allmatcheswatch(FootballMatchRepository $footballMatchRepository): Response
+    {
+        $footballMatch = $footballMatchRepository->findBy(['statut' => 'En cours']);
+        $footballMatch2 = $footballMatchRepository->findBy(['statut' => 'Prochainement']);
+        $footballMatch3 = $footballMatchRepository->findBy(['statut' => 'Terminé']);
+
+        return $this->render('allmatcheswatch.html.twig',[
+            'footballMatches' => $footballMatch,
+             'footballMatches2' => $footballMatch2,
+            'footballMatches3' => $footballMatch3,
+        ]);
+
+    }
+
+    #[Route('/onematchwatch/{id}', name:'visualiserunmatch', methods: ['GET'])]
+    public function Onematchwatch(FootballMatchRepository $footballMatchRepository, int $id): Response
+    {
+        $footballMatch = $footballMatchRepository->findOneBy(["id" =>$id]);
+
+        return $this->render('onematchwatch.html.twig', [
+
+            'footballMatch' => $footballMatch,
+        ]);
+
+    }
+
+    #[Route('betmatch', name:'miser')]
+    public function BetMatch(): Response
+    {
+
+        return $this->render('betmatch.html.twig');
+
+    }
+
+    #[Route('betallmatches', name:'parier')]
+    public function BetAllMatches(): Response
+    {
+
+        return $this->render('betallmatches.html.twig');
+
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -22,37 +22,6 @@ class HomeController extends AbstractController
 
     }
 
-    #[Route('allmatcheswatch', name:'visualiserlesmatchs')]
-    public function Allmatcheswatch(): Response
-    {
-
-        return $this->render('allmatcheswatch.html.twig');
-
-    }
-
-    #[Route('onematchwatch', name:'visualiserunmatch')]
-    public function Onematchwatch(): Response
-    {
-
-        return $this->render('onematchwatch.html.twig');
-
-    }
-
-    #[Route('betmatch', name:'miser')]
-    public function BetMatch(): Response
-    {
-
-        return $this->render('betmatch.html.twig');
-
-    }
-
-    #[Route('betallmatches', name:'parier')]
-    public function BetAllMatches(): Response
-    {
-
-        return $this->render('betallmatches.html.twig');
-
-    }
     #[Route('inscription', name:'inscription')]
     public function Inscription(): Response
     {
