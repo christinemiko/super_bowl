a16ea601ef99c632b95792fea0f13c44d854b09e
Christine Chau <christinemiko@gmail.com>
2023-06-11 18:00:29 +0200

    add button miser sur la selection if match statut prochainement

--- src/Controller/FootballMatchController.php
+++ src/Controller/FootballMatchController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\FootballMatch;
 use App\Repository\FootballMatchRepository;
+use App\Repository\FootballPlayerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -36,22 +37,34 @@ class FootballMatchController extends AbstractController
     }
 
     #[Route('/onematchwatch/{id}', name:'visualiserunmatch', methods: ['GET'])]
-    public function Onematchwatch(FootballMatchRepository $footballMatchRepository, int $id): Response
+    public function Onematchwatch(FootballMatchRepository $footballMatchRepository, FootballPlayerRepository $footballPlayerRepository,  int $id): Response
     {
         $footballMatch = $footballMatchRepository->findOneBy(["id" =>$id]);
+
+        $team1 = $footballMatch->getTeam1();
+        $team2 = $footballMatch->getTeam2();
+
+        $footballPlayers1 = $footballPlayerRepository->findBy(["team" => $team1]);
+        $footballPlayers2 = $footballPlayerRepository->findBy(["team" => $team2]);
 
         return $this->render('onematchwatch.html.twig', [
 
             'footballMatch' => $footballMatch,
+            'footballPlayers1' => $footballPlayers1,
+            'footballPlayers2' => $footballPlayers2,
         ]);
 
     }
 
-    #[Route('betmatch', name:'miser')]
-    public function BetMatch(): Response
+    #[Route('betmatch/{id}', name:'miser', methods: ['GET'])]
+    public function BetMatch(FootballMatchRepository $footballMatchRepository, int $id): Response
     {
+        $footballMatch = $footballMatchRepository->findOneBy(["id" =>$id]);
 
-        return $this->render('betmatch.html.twig');
+        return $this->render('betmatch.html.twig', [
+
+            'footballMatch' => $footballMatch,
+        ]);
 
     }
 
