1cca3fd00c5fab27f34c71884d72877988a9f10e
Christine Chau <christinemiko@gmail.com>
2023-06-17 23:03:00 +0200

    install easyadmin dashboard

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -9,16 +9,5 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin', methods: ['GET'])]
-    public function index(SportbetRepository $sportbetRepository): Response
-    {
-        $user = $this->getUser();
-        $sportbet = $sportbetRepository->findBy(['user' => $user], ['id' => 'DESC']);
 
-        return $this->render('myaccount.html.twig',[
-
-            'sportbets'=> $sportbet,
-
-        ]);
-    }
 }
