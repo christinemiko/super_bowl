0585db04a96f7faa0973b1494b36c7edcbd4fa10
Christine Chau <christinemiko@gmail.com>
2023-06-18 15:08:20 +0200

    add crud controllers all entities

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\FootballMatch;
+use App\Entity\FootballPlayer;
+use App\Entity\Sportbet;
+use App\Entity\Team;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -28,22 +32,41 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Super Bowl');
+            ->setTitle('SUPER BOWL');
     }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::section('Administration');
 
-        yield MenuItem::section('Match', 'fa-solid fa-football');
-        yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
+        yield MenuItem::section('Matches', 'fa-solid fa-football');
+        yield MenuItem::subMenu('', 'fas fa-bars')->setSubItems([
             menuItem::linkToCrud('Create FootballMatch', 'fas fa-plus', FootballMatch::class)->setAction(Crud::PAGE_NEW),
             menuItem::linkToCrud('Show FootballMatches', 'fas fa-eye', FootballMatch::class),
         ]);
 
-        yield MenuItem::section('Joueur de Football');
+        yield MenuItem::section('FootballMatch Players','');
+        yield MenuItem::subMenu('', 'fas fa-bars')->setSubItems([
+            menuItem::linkToCrud('Create Football Player', 'fas fa-plus', FootballPlayer::class)->setAction(Crud::PAGE_NEW),
+            menuItem::linkToCrud('Show Football Players', 'fas fa-eye', FootballPlayer::class),
+        ]);
 
-        yield MenuItem::section('Equipes');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::section('Teams', 'fa-solid fa-people-group');
+        yield MenuItem::subMenu('', 'fas fa-bars')->setSubItems([
+            menuItem::linkToCrud('Create Team', 'fas fa-plus', Team::class)->setAction(Crud::PAGE_NEW),
+            menuItem::linkToCrud('Show Teams', 'fas fa-eye', Team::class),
+        ]);
+
+        yield MenuItem::section('Sport Bets', '');
+        yield MenuItem::subMenu('', 'fas fa-bars')->setSubItems([
+            menuItem::linkToCrud('Create Sport bet', 'fas fa-plus', Sportbet::class)->setAction(Crud::PAGE_NEW),
+            menuItem::linkToCrud('Show Sport Bets', 'fas fa-eye', Sportbet::class),
+        ]);
+
+        yield MenuItem::section('Users');
+        yield MenuItem::subMenu('', 'fas fa-bars')->setSubItems([
+            menuItem::linkToCrud('Create User', 'fas fa-plus', User::class)->setAction(Crud::PAGE_NEW),
+            menuItem::linkToCrud('Show Users', 'fas fa-eye', User::class),
+        ]);
     }
 }
